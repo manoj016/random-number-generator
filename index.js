ed609acff6c8aa54f5605630b05e917cdf9af80e
manoj016 <manoj@quartic.ai>
2021-08-25 10:51:20 +0530

    fix(comment): add comment

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/**
+ * function to generate random number
+ *
+ * @param min - minimum number to generate
+ * @param max - maximum number to generate
+ * @returns {number}
+ */
 function randomNoGenerator(min, max) {
   if(typeof(max) !== 'number' && typeof(min) !== 'number') {
     min = 0;  max = 1;
